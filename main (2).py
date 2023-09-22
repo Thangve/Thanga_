dc82889c914cda996206787b62372988b4448f94
Thangve <144414478+Thangve@users.noreply.github.com>
2023-09-22 09:11:16 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,6 +1,15 @@
-import calendar 
-yr=int(input("enter year"))
-if calendar.isleap(yr):
-  print(yr,"is a leap year")
-else:
-  print(yr,"is a not leap year")
+def linearSearchProduct(productList, targetProduct):
+    indices = []
+
+    for index, product in enumerate(productList):
+        if product == targetProduct:
+            indices.append(index)
+    
+    return indices
+
+# Example usage
+products = ["shoes", "boat", "loafer", "sandal", "shoes"]
+target = "shoes"
+target2 = "apples"
+result = linearSearchProduct(products, target)
+print(result)
--- main (3).py
+++ main (3).py
@@ -1,10 +1,20 @@
-num=int(input("enter a num"))
-fact=1
-if num < 0:
-  print("negative num")
-elif num ==0:
-  print("the factorial of a seven number 0 to 1")
-else:
- for i in range (1,num+1):
-  fact=fact*i
-print(fact)
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+students = [
+    Student("Hari", "A123", 7.8),
+    Student("Srikanth", "A124", 8.9),
+    Student("Saumya", "A125", 9.1),
+    Student("Mahidhar", "A126", 9.9)
+]
+
+sorted_students = sort_students(students)
+for student in sorted_students:
+    print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name, student.roll_number, student.cgpa))
